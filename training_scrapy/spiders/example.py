051f65344a989d5a8712fbf45d0208f5e1a48320
Eugene Kolotikov <jeka.pxb@yandex.ru>
2024-08-08 22:47:30 +0300

    3

--- training_scrapy/spiders/example.py
+++ training_scrapy/spiders/example.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from training_scrapy.items import QuoteItem
+
 
 class QuotesSpider(scrapy.Spider):
     # Имя паука должно быть уникальным в рамках одного проекта.
@@ -12,11 +14,12 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css('div.quote'):
             # Для каждой найденной цитаты создаём и возвращаем словарь:
-            yield {
+            data = {
                 'text': quote.css('span.text::text').get(),
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('a.tag::text').getall(),
             }
+            yield QuoteItem(data)
 
         # По CSS-селектору ищем ссылку на следующую страницу.
         next_page = response.css('li.next a::attr(href)').get()
